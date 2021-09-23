0bf74211d6922ed71bb5851ec45300e2b1b76320
rodriguezl <Luis.Rodriguez@innovation.group>
2021-09-23 16:37:19 +0200

    Renamed Application run class

--- src/main/java/com/appraisaltool/Evaluation360Application.java
+++ src/main/java/com/appraisaltool/Evaluation360Application.java
@@ -5,12 +5,11 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
-//@SpringBootApplication
 @SpringBootApplication(exclude = { SecurityAutoConfiguration.class })
 @EnableJpaAuditing
-public class Application{
+public class Evaluation360Application{
 	public static void main(String[] args) {
-		SpringApplication.run(Application.class, args);
+		SpringApplication.run(Evaluation360Application.class, args);
 				
 	}
 	
