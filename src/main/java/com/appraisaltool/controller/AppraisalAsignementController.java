9ba78495bf44870964a7cb5dbdca461803392536
rodriguezl <Luis.Rodriguez@innovation.group>
2021-10-11 17:17:07 +0200

    REFACTOR: created a new controller and moved some methods

--- src/main/java/com/appraisaltool/controller/AppraisalAsignementController.java
+++ src/main/java/com/appraisaltool/controller/AppraisalAsignementController.java
@@ -15,7 +15,6 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 import com.appraisaltool.dto.AppraiserAssignementDto;
-import com.appraisaltool.model.Appraisal;
 import com.appraisaltool.request.AppraisalRequest;
 import com.appraisaltool.response.GGResponse;
 import com.appraisaltool.service.AppraisalService;
@@ -26,7 +25,7 @@ import io.swagger.annotations.ApiOperation;
         "http://localhost:4200", "http://localhost:4201"}, maxAge = 3600)
 @RestController
 @RequestMapping(value = MAPPING_APPRAISAL_TOOL + "/" + TAG_APPRAISAL_ASIG)
-@Api(value = "Appraisal functions", tags = TAG_APPRAISAL_ASIG, description = "Appraisal Asgignement functions")
+@Api(value = "Appraisal Asignement functions", tags = TAG_APPRAISAL_ASIG, description = "Appraisal Asgignement functions")
 public class AppraisalAsignementController {
 
     @Autowired
@@ -40,21 +39,6 @@ public class AppraisalAsignementController {
                 .getEvalDate()), null, true), HttpStatus.OK);
     }
 
-    @ApiOperation(tags = TAG_APPRAISAL_ASIG, value = "Get specific apraisal")
-    @RequestMapping(value = "/{appraisalId}", produces = {
-            MediaType.APPLICATION_JSON_VALUE}, headers = ACCEPT_APPLICATION_JSON, method = RequestMethod.GET)
-    public ResponseEntity<GGResponse<Appraisal>> getAppraisal(@PathVariable Integer appraisalId) {
-        return new ResponseEntity<GGResponse<Appraisal>>(new GGResponse<Appraisal>(appraisalService.getAppraisalById(appraisalId), null, true), HttpStatus.OK);
-    }
-
-    @ApiOperation(tags = TAG_APPRAISAL_ASIG, value = "delete specific apraisal")
-    @RequestMapping(value = "/{appraisalId}", produces = {
-            MediaType.APPLICATION_JSON_VALUE}, headers = ACCEPT_APPLICATION_JSON, method = RequestMethod.DELETE)
-    public ResponseEntity<GGResponse<String>> deleteAppraisal(@PathVariable Integer appraisalId) {
-        appraisalService.deleteAppraisal(appraisalId);
-
-        return new ResponseEntity<GGResponse<String>>(new GGResponse<String>("Delete sucessful", null, true), HttpStatus.OK);
-    }
 
     @ApiOperation(tags = TAG_APPRAISAL_ASIG, value = "Create an appraisal assignment to evaluate to all employed from one office")
     @RequestMapping(value = "/office/{officeId}", produces = {
